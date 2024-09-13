3542ecf630c255f67bd875e5c12b718ebd432878
Caden <cwmaster89@gmail.com>
2024-09-13 14:40:56 -0400

    message

--- write_ids.c
+++ write_ids.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 int main() {
-    char GITHUB_ID[] = "Fill_in_Your_Github_Id";
-    char STUDENT_ID[] = "Fill_in_Your_Student_Id";
+    char GITHUB_ID[] = "Ignitor8";
+    char STUDENT_ID[] = "cvw5788";
 
     return 0 ;
 }
